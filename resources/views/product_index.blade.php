47ee937e8bf5e2bd8f121e7d535937cb6e0392da
s-yukino <y.shoji175@gmail.com>
2022-01-20 01:38:40 +0900

    make controllers/models/databace/views

---
+++ resources/views/product_index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>商品一覧</title>
+</head>
+<body>
+    <p>検索フォーム</p>
+    <input type="search" value="商品名"></input>
+    <select name="companies">
+        <option value="サンプル1">サンプル1</option>
+        <option value="サンプル2">サンプル2</option>
+        <option value="サンプル3">サンプル3</option>
+    </select>
+    <button>検索</button>
+    <button>新規登録</button>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>商品画像</th>
+            <th>商品名</th>
+            <th>価格</th>
+            <th>在庫数</th>
+            <th>メーカー名</th>
+            <th>詳細表示</th>
+            <th>消去</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('product_index');
+})->middleware(['auth'])->name('product_index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
